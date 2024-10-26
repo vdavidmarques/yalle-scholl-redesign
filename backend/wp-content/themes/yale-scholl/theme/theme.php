f469beb14641de7cc64470c3e24f36a8859d94c4
vinicius <vdavidmarques@gmail.com>
2024-10-25 21:28:43 -0300

    cho:re Forcing acf field type image manually

--- backend/wp-content/themes/yale-scholl/theme/theme.php
+++ backend/wp-content/themes/yale-scholl/theme/theme.php
@@ -23,4 +23,21 @@
         ));
     }
     add_action('after_setup_theme', 'theme_custom_logo_setup');
+
+    function add_acf_fields_to_rest_api($response, $post, $request) {
+        if (function_exists('get_fields')) {
+            // Obter todos os campos ACF do post
+            $acf_fields = get_fields($post->ID);
+            
+            // Adicionar campos ACF aos dados da resposta
+            if ($acf_fields) {
+                $response->data['acf'] = $acf_fields;
+            }
+        }
+        return $response;
+    }
+    
+    add_filter('rest_prepare_post', 'add_acf_fields_to_rest_api', 10, 3);
+    add_filter('rest_prepare_page', 'add_acf_fields_to_rest_api', 10, 3);
+    
     
