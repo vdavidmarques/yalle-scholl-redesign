110bc815cee93454853c3c7a8264838bf62afb83
vinicius <vdavidmarques@gmail.com>
2024-10-26 13:32:47 -0300

    hotfix: Exposing ACF fields to REST API at courses custom post

--- backend/wp-content/themes/yale-scholl/theme/theme.php
+++ backend/wp-content/themes/yale-scholl/theme/theme.php
@@ -1,43 +1,62 @@
 <?php
-    /*******************************
+
+/*******************************
     Adding scripts and Css
-        ********************************/   
-    add_action( 'wp_enqueue_scripts', function () {
-        if (!is_admin()) {
-            wp_enqueue_style( 'style', get_template_directory_uri() . '/style.css', array(), '1.0.0' );
-            
-            wp_enqueue_script( 'script-js', get_template_directory_uri() . "/dist/library/js/scripts.min.js", array( 'jquery' ), null, true );
-        }
-    }); 
+ ********************************/
+add_action('wp_enqueue_scripts', function () {
+    if (!is_admin()) {
+        wp_enqueue_style('style', get_template_directory_uri() . '/style.css', array(), '1.0.0');
 
-    /*******************************
+        wp_enqueue_script('script-js', get_template_directory_uri() . "/dist/library/js/scripts.min.js", array('jquery'), null, true);
+    }
+});
+
+/*******************************
         Adding logo
-    ********************************/   
+ ********************************/
 
-    function theme_custom_logo_setup() {
-        add_theme_support('custom-logo', array(
-            'height'      => 100, 
-            'width'       => 300, 
-            'flex-height' => true,
-            'flex-width'  => true,
-        ));
-    }
-    add_action('after_setup_theme', 'theme_custom_logo_setup');
+function theme_custom_logo_setup()
+{
+    add_theme_support('custom-logo', array(
+        'height'      => 100,
+        'width'       => 300,
+        'flex-height' => true,
+        'flex-width'  => true,
+    ));
+}
+add_action('after_setup_theme', 'theme_custom_logo_setup');
 
-    function add_acf_fields_to_rest_api($response, $post, $request) {
-        if (function_exists('get_fields')) {
-            // Obter todos os campos ACF do post
-            $acf_fields = get_fields($post->ID);
-            
-            // Adicionar campos ACF aos dados da resposta
-            if ($acf_fields) {
-                $response->data['acf'] = $acf_fields;
-            }
+
+/*******************************
+        Exposing ACF fields to REST API
+ ********************************/
+function add_acf_fields_to_rest_api($response, $post)
+{
+    if (function_exists('get_fields')) {
+        // Obter todos os campos ACF do post
+        $acf_fields = get_fields($post->ID);
+
+        // Adicionar campos ACF aos dados da resposta
+        if ($acf_fields) {
+            $response->data['acf'] = $acf_fields;
         }
-        return $response;
     }
-    
-    add_filter('rest_prepare_post', 'add_acf_fields_to_rest_api', 10, 3);
-    add_filter('rest_prepare_page', 'add_acf_fields_to_rest_api', 10, 3);
-    
-    
+    return $response;
+}
+
+add_filter('rest_prepare_post', 'add_acf_fields_to_rest_api', 10, 3);
+
+function add_acf_fields_to_custom_post_api($response, $post, $request) {
+    if (function_exists('get_fields')) {
+        // Obter todos os campos ACF do post
+        $acf_fields = get_fields($post->ID);
+
+        // Adicionar campos ACF aos dados da resposta
+        if ($acf_fields) {
+            $response->data['acf'] = $acf_fields;
+        }
+    }
+    return $response;
+}
+
+add_filter('rest_prepare_courses', 'add_acf_fields_to_custom_post_api', 10, 3);
