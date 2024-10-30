466ccfe10759ba1caab9c8017e65ea33949332a8
vinicius <vdavidmarques@gmail.com>
2024-10-29 22:54:38 -0300

    chore: Updating  Exposing ACF fields to REST API function

--- backend/wp-content/themes/yale-scholl/theme/theme.php
+++ backend/wp-content/themes/yale-scholl/theme/theme.php
@@ -30,28 +30,10 @@ add_action('after_setup_theme', 'theme_custom_logo_setup');
 /*******************************
         Exposing ACF fields to REST API
  ********************************/
-function add_acf_fields_to_rest_api($response, $post)
-{
+function add_acf_fields_to_custom_post_api($response, $post) {
     if (function_exists('get_fields')) {
-        // Obter todos os campos ACF do post
         $acf_fields = get_fields($post->ID);
 
-        // Adicionar campos ACF aos dados da resposta
-        if ($acf_fields) {
-            $response->data['acf'] = $acf_fields;
-        }
-    }
-    return $response;
-}
-
-add_filter('rest_prepare_post', 'add_acf_fields_to_rest_api', 10, 3);
-
-function add_acf_fields_to_custom_post_api($response, $post, $request) {
-    if (function_exists('get_fields')) {
-        // Obter todos os campos ACF do post
-        $acf_fields = get_fields($post->ID);
-
-        // Adicionar campos ACF aos dados da resposta
         if ($acf_fields) {
             $response->data['acf'] = $acf_fields;
         }
@@ -60,3 +42,31 @@ function add_acf_fields_to_custom_post_api($response, $post, $request) {
 }
 
 add_filter('rest_prepare_courses', 'add_acf_fields_to_custom_post_api', 10, 3);
+
+function add_acf_fields_to_rest_api($response, $post, $request) {
+    if (function_exists('get_fields')) {
+        $acf_fields = get_fields($post->ID);
+
+        if ($acf_fields) {
+            foreach ($acf_fields as $key => $value) {
+                if (is_numeric($value) && wp_attachment_is_image($value)) {
+                    $image_data = wp_get_attachment_image_src($value, 'full');
+                    $acf_fields[$key] = [
+                        'id' => $value,
+                        'url' => $image_data[0],
+                        'width' => $image_data[1],
+                        'height' => $image_data[2],
+                    ];
+                }
+            }
+
+            $response->data['acf'] = $acf_fields;
+        }
+    }
+
+    return $response;
+}
+
+// Aplica a função para páginas e posts (e custom post types)
+add_filter('rest_prepare_page', 'add_acf_fields_to_rest_api', 10, 3);
+add_filter('rest_prepare_post', 'add_acf_fields_to_rest_api', 10, 3);
