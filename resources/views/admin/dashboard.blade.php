b0d28a509fb6d0a815f6e841136ef9092c4f6ad9
vinicius <vdavidmarques@gmail.com>
2025-04-23 23:38:24 -0300

    feat: Dashboard - controller, views files

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.admin')
+
+@section('content')
+    <h1 class="text-2xl font-semibold mb-4">Bem-vindo ao Painel Administrativo!</h1>
+    <p>Aqui é onde você vai gerenciar as coisas do site da escola de artes ✨</p>
+@endsection
