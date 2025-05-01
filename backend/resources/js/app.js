01763d15e0892677218c8b5a9a6f57a04273a98f
vdavidmarques <vdavidmarques@gmail.com>
2025-04-30 21:14:55 -0300

    chore: changing folder

--- backend/resources/js/app.js
+++ backend/resources/js/app.js
@@ -1,12 +1,1 @@
 import './bootstrap';
-
-import Alpine from 'alpinejs';
-
-import "trix";
-
-import { createIcons, icons } from 'lucide';
-createIcons({icons});
-
-window.Alpine = Alpine;
-
-Alpine.start();
